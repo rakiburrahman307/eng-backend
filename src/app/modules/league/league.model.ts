3bdc496dbd7b2ae98ede1f232dee0573e57fc7fa
rakiburrahman307 <rakiburrahman307@gmail.com>
2026-08-04 17:15:50 +0600

    feat: initialize Mongoose League model with required schema fields

--- src/app/modules/league/league.model.ts
+++ src/app/modules/league/league.model.ts
@@ -5,21 +5,25 @@ const leagueSchema = new Schema(
     leagueName: {
       type: String,
       required: true,
+      trim: true,
     },
 
     season: {
       type: String,
       required: true,
+      trim: true,
     },
 
     startDate: {
       type: Date,
       required: true,
+      trim: true,
     },
 
     endDate: {
       type: Date,
       required: true,
+      trim: true,
     },
 
     // status: {
